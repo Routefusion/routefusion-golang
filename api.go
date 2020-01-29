cfd890ea9911f37d038132702dac173beaca1c08
Sudarsan <sudar.theone@gmail.com>
2020-01-29 16:38:04 +0800

    api: completed the beneficiaries endpoints on the interface

--- api.go
+++ api.go
@@ -1,6 +1,10 @@
 package routefusion
 
 // Client specifies the abstraction for Routefusion APIs.
+// TODO: Question - Should the client be a unified interface or can I split
+// it up into sub interfaces like Users, Beneficiaries and embded them in
+// the unified interface? This adds a bit of overhead to the client while
+// calling which is why I only want to do it if absolutely needed.
 type Client interface {
 	GetUser() (*UserDetails, error)
 	UpdateUser(*User) (*UpdatedUserDetails, error)
@@ -12,4 +16,8 @@ type Client interface {
 	GetBeneficiary(id string) (*BeneficiaryBase, error)
 	CreateBeneficiary(*BeneficiaryInput) (*BeneficiaryBase, error)
 	UpdateBeneficiary(id string, body *UpdateBeneficiaryInput) (*BeneficiaryBase, error)
+	GetSubUserBeneficiariesMaster(subuserID string) ([]Beneficiary, error)
+	GetSubUserBeneficiaryMaster(subuserID string, beneficiaryID string) (*BeneficiaryBase, error)
+	CreateSubUserBeneficiaryMaster(subUserID string) (*BeneficiaryBase, error)
+	UpdateSubUserBeneficiaryMaster(subUserID string, beneficiaryID string) (*BeneficiaryBase, error)
 }
