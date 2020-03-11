09e7d9f8c90ad957dd9579e439467aacb46f3fac
Sudarsan <sudar.theone@gmail.com>
2020-03-11 10:46:55 +0800

    add generator interface

---
+++ internal/generator/generator.go
@@ -0,0 +1,41 @@
+package generator
+
+import "io"
+
+// Parameter is a parameter attached to a function.
+type Parameter struct {
+	Name string
+	Type string
+}
+
+// API is a set of indicatives that we will loop through
+// to generate code.
+type API struct {
+	InterfaceName string
+	Methods       []Method
+}
+
+// Method represents a set of metadata levels details that belong
+// to an api method.
+type Method struct {
+	Endpoint     string
+	MethodName   string
+	InputParams  []Parameter
+	OutputParams []Parameter
+}
+
+// Reader is a general wrapper on io.Reader to read an input to obtain
+// the apis.
+type Reader interface {
+	ReadAPI(io.Reader) ([]API, error)
+}
+
+// Writer is a general wrapper on io.Writer to write the processed file.
+type Writer interface {
+	WriteAPI(io.Writer, []API) error
+}
+
+// Generator is an interface that specifies how a generation would occur.
+type Generator interface {
+	Copy(io.Reader, io.Writer) error
+}
